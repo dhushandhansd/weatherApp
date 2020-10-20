4aadb9c83aee947d4f80652a52093dba47e3dbaf
Shree Dhushandhan <dhushandhan.sd@gmail.com>
2020-10-20 17:33:58 +0530

    Updates

---
+++ precache-manifest.381fa2a93ebcacbc1a4931f91404a249.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138682b82c556a0d993f2fbb4ba736c8",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "95b750f5d117e9c3aa95",
+    "url": "/weather-app/static/css/main.a4c63d20.chunk.css"
+  },
+  {
+    "revision": "9756372c0e3197f07447",
+    "url": "/weather-app/static/js/2.4a75e904.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.4a75e904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95b750f5d117e9c3aa95",
+    "url": "/weather-app/static/js/main.43991224.chunk.js"
+  },
+  {
+    "revision": "965d95ca7eba495cb2ea",
+    "url": "/weather-app/static/js/runtime-main.38e1c25c.js"
+  },
+  {
+    "revision": "5dd081e62e490e5d32cf85bdc26ac9eb",
+    "url": "/weather-app/static/media/NormalPage.5dd081e6.png"
+  },
+  {
+    "revision": "a40dd5ddf62ab859ac3fea70f19ab4be",
+    "url": "/weather-app/static/media/cloudybg.a40dd5dd.png"
+  },
+  {
+    "revision": "1e40910bf32aee4eb819c5b46f9479fc",
+    "url": "/weather-app/static/media/mildfogimg.1e40910b.png"
+  },
+  {
+    "revision": "9617695cde5d5a2e8dee453da7295a30",
+    "url": "/weather-app/static/media/mildsunbg.9617695c.png"
+  },
+  {
+    "revision": "30d35f34d9d0716601d09c49f7795d6f",
+    "url": "/weather-app/static/media/snowbg.30d35f34.png"
+  },
+  {
+    "revision": "fc2e465e030f3ba2d9e8dd44ac2bc69d",
+    "url": "/weather-app/static/media/snowflake.fc2e465e.png"
+  },
+  {
+    "revision": "d4c47d99e80e93311149aa9f5de618b3",
+    "url": "/weather-app/static/media/stormimg.d4c47d99.png"
+  },
+  {
+    "revision": "d4072caa6c6b09adad21b89088f9f8fb",
+    "url": "/weather-app/static/media/sunnybg.d4072caa.png"
+  },
+  {
+    "revision": "e566f3570e7ab21696fcacb3b9e2b76c",
+    "url": "/weather-app/static/media/sunnyimg.e566f357.png"
+  }
+]);
